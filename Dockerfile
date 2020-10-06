c4efa1a456dc27607cc3c50a8d9ae1483c0d7cf1
Jayesh Malhotra <jayeshmal96@gmail.com>
2020-10-06 21:46:56 +1100

    s3 cmd

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY dobackup.sh /
 RUN \
 	mkdir -p /aws && \
 	apk -Uuv add groff less python3 py3-pip && \
-	pip3 install awscli && \
+	pip3 install awscli s3cmd && \
 	apk --purge -v del py-pip && \
 	rm /var/cache/apk/* && \
 	chmod +x /entrypoint.sh /dobackup.sh
--- dobackup.sh
+++ dobackup.sh
@@ -4,12 +4,14 @@
 S3_STORAGE_CLASS=${S3_STORAGE_CLASS:-STANDARD}
 
 # generate file name for tar
-FILE_NAME=/tmp/$BACKUP_NAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz
+# FILE_NAME=/tmp/$BACKUP_NAME-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz
 
-echo "creating archive"
-tar -zcvf $FILE_NAME /data
-echo "uploading archive to S3 [$FILE_NAME, storage class - $S3_STORAGE_CLASS]"
-aws s3 cp --storage-class $S3_STORAGE_CLASS $FILE_NAME $S3_BUCKET_URL
-echo "removing local archive"
-rm $FILE_NAME
+# echo "creating archive"
+# tar -zcvf $FILE_NAME /data
+# echo "uploading archive to S3 [$FILE_NAME, storage class - $S3_STORAGE_CLASS]"
+# aws s3 cp --storage-class $S3_STORAGE_CLASS $FILE_NAME $S3_BUCKET_URL
+# echo "removing local archive"
+# rm $FILE_NAME
+echo "synchronising files to S3"
+s3cmd sync --dry-run --storage-class $S3_STORAGE_CLASS /data/ $S3_BUCKET_URL
 echo "done"
